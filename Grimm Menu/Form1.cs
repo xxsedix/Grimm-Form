32f17ae4af9e0aa2e9cda87b4cc9ba52550f4c0e
xxsedix <dr.xxsedix@gmail.com>
2022-03-22 20:43:48 -0500

    Add project files.

---
+++ Grimm Menu/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Grimm_Menu
+{
+    public partial class Form1 : Form
+    {
+        private MenuStrip mainMenu;
+        public Form1()
+        {
+            InitializeComponent();
+            mainMenu = new MenuStrip();
+            MainMenuStrip = mainMenu;
+            Controls.Add(mainMenu);
+
+            ToolStripMenuItem mnuFile = new ToolStripMenuItem("File");
+        }
+    }
+}
